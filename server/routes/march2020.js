a9b261b8f0eef0e8de3fdb27a713d6f5df203e10
Joe Tseng <jtsengorg@gmail.com>
2022-04-24 23:07:10 -0400

    Calculates the day of March 2020.

--- server/routes/march2020.js
+++ server/routes/march2020.js
@@ -5,7 +5,9 @@ var dayjs = require('dayjs');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   let now = dayjs();
-  res.send({'today': now.format()});
+  // res.send({'today': now.format()});
+  march2020AsEpoch = dayjs('2020-03-01').unix()
+  res.send({'march2020': parseInt((now.unix() - march2020AsEpoch)/86400)});
 });
 
 module.exports = router;

cdc780aa588ca14c881ea00300fd3085c84b6dfa
Joe Tseng <jtsengorg@gmail.com>
2022-05-19 15:43:15 -0400

    Added jest tests.

---
+++ frontend/src/App.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import logo from './logo.svg';
+import './App.css';
+
+let SignInForm = props => {
+  const { handleSubmit } = props;
+  return <form onSubmit={handleSubmit} className="form">
+    <div className="field">
+      <div className="control">
+        <Field name="firstName" component={renderField} type="text" label="First Name"/>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <Field name="lastName" component={renderField} type="text" label="Last Name"/>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <Field name="email" component={renderField} type="email" label="Email Address"/>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <Field name="age" component={renderField} type="number" label="Age"/>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <label className="label">Proficiency</label>
+        <div className="select">
+          <Field className="input" name="proficiency" component="select">
+            <option />
+            <option value="beginner">Beginner Dev</option>
+            <option value="intermediate">Intermediate Dev</option>
+            <option value="expert">Expert Dev</option>
+          </Field>
+        </div>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <label className="label">Gender</label>
+        <label className="radio">
+          <Field name="gender" component="input" type="radio" value="male" />
+          {' '}
+          Male
+        </label>
+        <label className="radio">
+          <Field name="gender" component="input" type="radio" value="female" />
+          {' '}
+          Female
+        </label>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <label className="checkbox">
+          <Field name="saveDetails" id="saveDetails" component="input" type="checkbox"/>
+          Save Details
+        </label>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <label className="label">Message</label>
+        <Field className="textarea" name="message" component="textarea" />
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <button className="button is-link">Submit</button>
+      </div>
+    </div>
+
+  </form>;
+};
+
+const validate = val => {
+  const errors = {};
+  if (!val.firstName) {
+    console.log('First Name is required');
+    errors.firstName = 'Required';
+  }
+  if (!val.lastName) {
+    console.log('Last Name is required');
+    errors.lastName = 'Required';
+  }
+  if (!val.email) {
+    console.log('email is required');
+    errors.email = 'Required';
+  } else if (!/^.+@.+$/i.test(val.email)) {
+    console.log('email is invalid');
+    errors.email = 'Invalid email address';
+  }
+  if (!val.age) {
+    errors.age = 'Required'
+  } else if (isNaN(Number(val.age))) {
+    errors.age = 'Must be a number'
+  } else if (Number(val.age) < 18) {
+    errors.age = 'Sorry, you must be at least 18 years old'
+  }
+  return errors;
+};
+
+const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+  <div>
+    <div className="control">
+      <label className="field">{label}</label>
+      <input className="input" {...input} placeholder={label} type={type}/>
+      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+)
+
+SignInForm = reduxForm({
+  form: 'signIn',
+  validate,
+})(SignInForm);
+
+class App extends Component {
+
+  handleSignIn = values => {
+    console.log(values);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React x redux-form</h1>
+        </header>
+        <div className="container">
+          <p className="App-intro">
+            Contact Form
+          </p>
+          {/* <SignInForm onSubmit={this.handleSignIn} /> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
